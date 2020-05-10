c4c912be4586802cef36dd2abf667f4c63109a80
Julie Rymer <rymerjulie.pro@gmail.com>
2020-05-10 12:18:17 +0200

    [feat] file props webservice

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
-from flask import Flask
+import os
+import stat
+import sys
 
-app = Flask(__name__)
+from flask import Flask, jsonify, request
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello World!'
+    return 'Hello, please go visit "/stat"'
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/stat")
+def pathstats():
+    rootdir = request.args.get("rootdir", os.getcwd())
+    maxlevel = int(request.args.get("maxlevel", 1))
+    stats = walktree(rootdir, maxlevel)
+    return jsonify(stats)
+
+
+def walktree(rootdir, maxlevel=None):
+    if maxlevel is not None:
+        if maxlevel <= 0:
+            return None
+        maxlevel -= 1
+
+    dirstats = {}
+    for f in os.listdir(rootdir):
+        pathname = os.path.join(rootdir, f)
+        mode = os.stat(pathname).st_mode
+        if stat.S_ISDIR(mode):
+            dirstats[f] = walktree(pathname, maxlevel)
+        elif stat.S_ISREG(mode):
+            dirstats[f] = filestats(pathname)
+    return dirstats
+
+
+def filestats(filepath):
+    stats = os.stat(filepath)
+    return {
+        prop: getattr(stats, prop)
+        for prop in dir(stats)
+        if prop.startswith("st_")
+    }
+
+
+if __name__ == "__main__":
+    rootdir = sys.argv[1] if len(sys.argv) == 2 else "."
+    from pprint import pprint as pp
+
+    pp(walktree(rootdir, 2))
